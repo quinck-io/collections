854132157464211589a6e6b6684e7a380f1b7313
Stefano Righini <stefano.righini@quinck.io>
2022-01-28 22:49:34 +0100

    [Arrays][GroupBy] add tests for Array.groupBy

---
+++ __tests__/arrays/groupBy.test.ts
@@ -0,0 +1,56 @@
+import '../../src/arrays/arrays'
+import {FoodCategory, foods, fruits} from './groupBy.utils'
+
+describe('Tests for Array.groupBy', () => {
+
+    test('should return a Map', () => {
+        const foodsByCategory = foods.groupBy(
+            ({category}) => category,
+            ({foodName}) => [foodName],
+            (a,b) => a.concat(b)
+        )
+
+        expect(foodsByCategory).toBeInstanceOf(Map)
+    })
+
+    test('should return the results with the correct keys', () => {
+
+        const foodsByCategory = foods.groupBy(
+            ({category}) => category,
+            ({foodName}) => [foodName],
+            (a,b) => a.concat(b)
+        )
+
+        expect(foodsByCategory.has(FoodCategory.FRUITS)).toBe(true)
+        expect(foodsByCategory.has(FoodCategory.VEGETABLES)).toBe(true)
+    })
+
+    test('should return the results with the correct values', () => {
+
+        const foodsByCategory = foods.groupBy(
+            ({category}) => category,
+            ({foodName}) => [foodName],
+            (a,b) => a.concat(b)
+        )
+
+        const groupedFruits = foodsByCategory.get(FoodCategory.FRUITS)
+        expect(groupedFruits).not.toBeNull()
+        expect(groupedFruits).not.toBeUndefined()
+        expect(groupedFruits).toBeInstanceOf(Array)
+        fruits.forEach(({foodName}) => expect(groupedFruits).toContain(foodName))
+    
+    })
+
+    test('should not add additional keys', () => {
+
+        const foodsByCategory = fruits.groupBy(
+            ({category}) => category,
+            ({foodName}) => [foodName],
+            (a,b) => a.concat(b)
+        )
+
+        expect(foodsByCategory.has(FoodCategory.VEGETABLES)).toBe(false)
+    
+    })
+
+})
