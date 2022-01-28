fbf7788bfae6a233010ec9dc63632eed678a5bc9
Stefano Righini <stefano.righini@quinck.io>
2022-01-28 22:04:38 +0100

    [Arrays][GroupBy] add simplified verions on groupby and groupbyToDictionary

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -20,6 +20,24 @@ declare global {
      * @returns an object with the elements grouped by the keys generated using the keyMapper
      */
     groupbyToDictionary<K extends string,V>(keyMapper: Mapper<T,K>, valueMapper: Mapper<T,V>, mergeFunction?: GroupByMergeFunction<V>): Record<K,V>
+    /**
+     * Perform a group by operation grouping by the result of the keyMapper.
+     * As the simplified version of a groupby this method only allows to 
+     * group by objects by specific keys generating an array of elements 
+     * for each key.
+     * @param keyMapper used to get the key
+     * @returns a map with the elements grouped by the keys generated using the keyMapper
+     */
+    simpleGroupby<K>(keyMapper: Mapper<T,K>): Map<K,T[]>
+    /**
+     * Perform a group by operation grouping by the result of the keyMapper.
+     * As the simplified version of a groupby this method only allows to 
+     * group by objects by specific keys generating an array of elements 
+     * for each key.
+     * @param keyMapper used to get the key
+     * @returns a map with the elements grouped by the keys generated using the keyMapper
+     */
+     simpleGroupbyToDictionary<K extends string>(keyMapper: Mapper<T,K>): Record<K,T[]>
   }
 }
 
@@ -51,4 +69,18 @@ Array.prototype.groupbyToDictionary = function <K extends string,V>(
         object[key] = prevValue ? mergeFunction(prevValue,value) : value
         return object
     },{} as Record<K,V>)
+}
+
+Array.prototype.simpleGroupby = function <K>(
+    keyMapper: Mapper<unknown,K>
+): Map<K,unknown[]> {
+    const _self = this as Array<unknown>
+    return _self.groupby(keyMapper, v => [v], (a,b) => a.concat(b))
+}
+
+Array.prototype.simpleGroupbyToDictionary = function <K extends string>(
+    keyMapper: Mapper<unknown,K>
+): Record<K,unknown[]> {
+    const _self = this as Array<unknown>
+    return _self.groupbyToDictionary(keyMapper, v => [v], (a,b) => a.concat(b))
 }
