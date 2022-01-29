6228a78963ea637876d44d3dba8698149b7e9658
Stefano Righini <stefano.righini@quinck.io>
2022-01-29 23:43:16 +0100

    [Map] add keysArray and valuesArray to get map keys and values as Array

--- src/maps/toArray.ts
+++ src/maps/toArray.ts
@@ -3,10 +3,22 @@ export { }
 declare global {
   interface Map<K,V> {
       toArray(): Array<[K,V]>
+      keysArray(): Array<K>
+      valuesArray(): Array<V>
   }
 }
 
 Map.prototype.toArray = function <K,V>(): Array<[K,V]> {
     const _self = this as Map<K,V>
     return Array.from(_self.entries())
+}
+
+Map.prototype.keysArray = function <K,V>(): Array<K> {
+    const _self = this as Map<K,V>
+    return Array.from(_self.keys())
+}
+
+Map.prototype.valuesArray = function <K,V>(): Array<V> {
+    const _self = this as Map<K,V>
+    return Array.from(_self.values())
 }
