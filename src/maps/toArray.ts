b70576f81ceabec69a46669198c39a40b3f190d4
Stefano Righini <stefano.righini@quinck.io>
2022-01-31 11:21:46 +0100

    [Map] add documentation for Map extension methods

--- src/maps/toArray.ts
+++ src/maps/toArray.ts
@@ -2,8 +2,22 @@ export { }
 
 declare global {
   interface Map<K,V> {
+      /**
+       * Creates a new Array with all the elements of the Map.
+       * Each element of the new Array will be 
+       * an Array with a Key and the relative Value.
+       * @returns an Array with the Map entries
+       */
       toArray(): Array<[K,V]>
+      /**
+       * Creates a new Array with all the keys of the Map.
+       * @returns an Array with the Map keys
+       */
       keysArray(): Array<K>
+      /**
+       * Creates a new Array with all the values of the Map.
+       * @returns an Array with the Map values
+       */
       valuesArray(): Array<V>
   }
 }
